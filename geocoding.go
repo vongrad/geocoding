3162266ad3d371d3d7c2e5db93802d33e15bc209
Adam Vongrej <vongrad@Adams-MacBook-Pro.local>
2017-10-05 20:33:04 +0200

    Initial commit

---
+++ geocoding.go
@@ -0,0 +1,93 @@
+package geocoding
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+const host = "https://maps.googleapis.com/maps/api/geocode/json"
+
+// API for interaction with Google Maps Geocoding API
+type API struct {
+	apiKey string
+}
+
+// Geocode returns the latitude and longitude given an address
+func (api API) Geocode(address string) (float64, float64, error) {
+
+	req, err := http.NewRequest("GET", host+"?address="+url.QueryEscape(address)+"&key="+api.apiKey, nil)
+
+	if err != nil {
+		return 0, 0, err
+	}
+
+	client := http.DefaultClient
+	res, err := client.Do(req)
+
+	if err != nil {
+		return 0, 0, err
+	}
+
+	var result Result
+
+	dec := json.NewDecoder(res.Body)
+
+	defer res.Body.Close()
+
+	if err := dec.Decode(&result); err != nil {
+		return 0, 0, err
+	}
+
+	if len(result.Results) == 0 {
+		return 0, 0, fmt.Errorf("No results found matching %v", address)
+	}
+
+	lat := result.Results[0].Geometry.Location.Latitude
+	lng := result.Results[0].Geometry.Location.Longitude
+
+	return lat, lng, nil
+}
+
+// Result acquired from Google Maps Geocoding API
+type Result struct {
+	Results []Address `json:"results"`
+}
+
+// Address is a single geocoded address
+type Address struct {
+	AddressComponents []AddressComponent `json:"address_components"`
+	FormattedAddress  string             `json:"formatted_address"`
+	Geometry          AddressGeometry    `json:"geometry"`
+	Types             []string           `json:"types"`
+	PlaceID           string             `json:"place_id"`
+}
+
+// AddressComponent is a part of an address
+type AddressComponent struct {
+	LongName  string   `json:"long_name"`
+	ShortName string   `json:"short_name"`
+	Types     []string `json:"types"`
+}
+
+// AddressGeometry is the location of a an address
+type AddressGeometry struct {
+	Location     LatLng       `json:"location"`
+	LocationType string       `json:"location_type"`
+	Bounds       LatLngBounds `json:"bounds"`
+	Viewport     LatLngBounds `json:"viewport"`
+	Types        []string     `json:"types"`
+}
+
+// LatLng is a tuple for latitude and longitude
+type LatLng struct {
+	Latitude  float64 `json:"lat"`
+	Longitude float64 `json:"lng"`
+}
+
+// LatLngBounds represents a bounded square area on the Earth.
+type LatLngBounds struct {
+	NorthEast LatLng `json:"northeast"`
+	SouthWest LatLng `json:"southwest"`
+}
---
+++ geocoding_test.go
@@ -0,0 +1,25 @@
+package geocoding
+
+import "testing"
+
+const (
+	address   string  = "1600 Amphitheatre Parkway, Mountain View, CA"
+	latitude  float64 = 37.4216548
+	longitude float64 = -122.0856374
+)
+
+func TestGeocode(t *testing.T) {
+
+	api := API{apiKey: ""}
+
+	lat, lng, err := api.Geocode(address)
+
+	if err != nil {
+		t.Errorf("1600 Amphitheatre Parkway, Mountain View, CA: Expected error to be nil, ~ Received %v", err)
+	}
+
+	if lat != latitude || lng != longitude {
+		t.Errorf("1600 Amphitheatre Parkway, Mountain View, CA: Expected (%f, %f), ~ Received (%f, %f)", latitude, longitude, lat, lng)
+	}
+
+}
